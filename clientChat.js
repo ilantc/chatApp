c10c193dd8b7af2ffbb0ef6f11a0a0b40cdd06f0
Ilan <ilantch@gmail.com>
2014-04-09 19:49:33 +0300

    adding send hat to server

--- clientChat.js
+++ clientChat.js
@@ -1,19 +1,26 @@
-/**
- * Created by User on 4/9/14.
- */
+(function () {
+    'use strict';
+    var inputBox = document.getElementById('inputBox');
+    var displayBox = document.getElementById('displayBox');
+    inputBox.focus();
+    inputBox.addEventListener('keyup', function (event) {
+        if ((event.keyCode == 13) && (inputBox.value.trim() != '')) {
+            var newChat = document.createElement('div');
+            newChat.textContent = inputBox.value;
+            newChat.setAttribute('class', 'c    hatMsg');
+            displayBox.appendChild(newChat);
+            sendChatToServer(inputBox.value);
+            // clear chatbox
+            inputBox.value = '';
+        }
+    });
 
-inputBox = document.getElementById('inputBox');
-displayBox = document.getElementById('displayBox');
-inputBox.focus();
-
-
-inputBox.addEventListener('keyup', function(event) {
-    if ((event.keyCode == 13) && (inputBox.value.trim() != '')) {
-        var newChat = document.createElement('div');
-        newChat.textContent = inputBox.value;
-        newChat.setAttribute('class','chatMsg');
-//        newChat.setAttribute('textContent',inputBox.value);
-        displayBox.appendChild(newChat);
-        inputBox.value = '';
+    function sendChatToServer(chatMsg) {
+        var request = new XMLHttpRequest();
+        request.onload = highscoreReceived;
+        request.open('post', '/chat');
+        request.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+        request.send('chatMsg');
     }
-});
+}());
+
